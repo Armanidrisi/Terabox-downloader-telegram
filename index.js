99d60adf4d6205b75d3362e706a81c070a7b235f
Arman Idrisi <119788224+Armanidrisi@users.noreply.github.com>
2024-02-20 12:37:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ async function main() {
 
   bot.start(async (ctx) => {
     ctx.reply(
-      `Hi ${ctx.message.from.first_name},\n\nI can Download Files from Terabox.\n\nMade with ❤️ by @Mohd_arman_idrisi01\n\nSend any terabox link to download.`,
+      `Hi ${ctx.message.from.first_name},\n\nI can Download Files from Terabox.\n\nMade with ❤️ by @botcodes123\n\nSend any terabox link to download.`,
       Markup.inlineKeyboard([
         Markup.button.url(" Channel", "https://t.me/botcodes123"),
         Markup.button.url("Report bug", "https://t.me/Armanidrisi_bot"),
